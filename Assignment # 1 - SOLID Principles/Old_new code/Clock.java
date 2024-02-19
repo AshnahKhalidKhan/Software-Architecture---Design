86f517fb672474fcd5570f17f52dd799c36adbd2
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2024-02-19 11:53:25 +0500

    Main class

--- Assignment # 1 - SOLID Principles/Old_new code/Clock.java
+++ Assignment # 1 - SOLID Principles/Old_new code/Clock.java
@@ -3,14 +3,59 @@ public class Clock
     private int hours;
     private int minutes;
     
+    /*
+        TEST CASES: --> Check karlena Ashhhhh
+        Clock(1, 20)
+        hours: 1, minutes: 20
+        
+        Clock(-1, 20)
+        hours: 23, minutes: 20
+        
+        Clock(1, -20)
+        hours: 1, minutes: 40
+        
+        Clock(-1, -20)
+        hours: 23, minutes: 40
+        
+        Clock(49, 20)
+        hours: 1, minutes: 20
+        
+        Clock(1, 70)
+        hours: 2, minutes: 10
+        
+        Clock(-1, 70) ------------------------> Confirm karao
+        hours: 0, minutes: 10
+        
+        Clock(49, -20) ------------------------> Confirm karao
+        hours: 1, minutes: 40
+        
+        Clock(1, -70) ------------------------> Ye to ghalat he lag raha hai...
+        1:00
+        1:-70
+        23:50
+        hours: 23, minutes: 50
+    */
+    
     public Clock (int hours, int minutes)
     {
-        if (hours < 0 || hours >= HOURS_IN_DAY) 
+        while (Math.abs(minutes) > 59)
         {
-            throw new IllegalArgumentException("Hours must be between 0 and " + (23));
+            if (minutes < 0)
+            {
+                hours--;
+                minutes = minutes + 60;
+            }
+            else //if minutes >= 0
+            {
+                hours++;
+                minutes = minutes - 60;
+            }
         }
-        this.hours = hours;
-        this.hours = hours;
-        this.minutes = minutes;
+        this.minutes = minutes % 60;
+    }
+    
+    public String toString() 
+    {
+        return this.hours + ":" + this.minutes + " hours: " + this.hours + " minutes: " + this.minutes;
     }
 }
---
+++ Assignment # 1 - SOLID Principles/Old_new code/Main.java
@@ -0,0 +1,65 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+        Clock c1 = new Clock(1, 20);
+        // hours: 1, minutes: 20
+        
+        Clock c2 = new Clock(-1, 20);
+        // hours: 23, minutes: 20
+        
+        Clock c3 = new Clock(1, -20);
+        // hours: 1, minutes: 40
+        
+        Clock c4 = new Clock(-1, -20);
+        // hours: 23, minutes: 40
+        
+        Clock c5 = new Clock(49, 20);
+        // hours: 1, minutes: 20
+        
+        Clock c6 = new Clock(1, 70);
+        // hours: 2, minutes: 10
+        
+        Clock c7 = new Clock(-1, 70);
+        // hours: 0, minutes: 10
+        
+        Clock c8 = new Clock(49, -20);
+        // hours: 1, minutes: 40
+        
+        Clock c9 = new Clock(1, -70);
+        // hours: 23, minutes: 50
+        
+        Clock c10 = new Clock(1, -130);
+        // hours: 22, minutes: 50
+        
+        System.out.println(c1.toString().equals("1:20 hours: 1 minutes: 20"));
+        System.out.println(c1 + "\n");
+        
+        System.out.println(c2.toString().equals("23:20 hours: 23 minutes: 20"));
+        System.out.println(c2 + "\n");
+        
+        System.out.println(c3.toString().equals("1:40 hours: 1 minutes: 40"));
+        System.out.println(c3 + "\n");
+        
+        System.out.println(c4.toString().equals("23:40 hours: 23 minutes: 40"));
+        System.out.println(c4 + "\n");
+        
+        System.out.println(c5.toString().equals("1:20 hours: 1 minutes: 20"));
+        System.out.println(c5 + "\n");
+        
+        System.out.println(c6.toString().equals("2:10 hours: 2 minutes: 10"));
+        System.out.println(c6 + "\n");
+        
+        System.out.println(c7.toString().equals("0:10 hours: 0 minutes: 10"));
+        System.out.println(c7 + "\n");
+        
+        System.out.println(c8.toString().equals("1:40 hours: 1 minutes: 40"));
+        System.out.println(c8 + "\n");
+        
+        System.out.println(c9.toString().equals("23:50 hours: 23 minutes: 50"));
+        System.out.println(c9 + "\n");
+        
+        System.out.println(c10.toString().equals("22:50 hours: 22 minutes: 50"));
+        System.out.println(c10 + "\n");
+	}
+}
